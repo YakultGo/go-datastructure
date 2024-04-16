b7f634ea8d8dd409f7f01337b4f14b80fee2f737
YakultGo <god1239426256@163.com>
2024-04-16 11:20:48 +0800

    前缀树

--- datastructure/trie.go
+++ datastructure/trie.go
@@ -1,39 +1,47 @@
 package datastructure
 
+import "runtime/debug"
+
+// 禁止GC，能够提高性能（参考灵神）
+func init() { debug.SetGCPercent(-1) }
+
 type Trie struct {
-    nxt [26]*Trie
-    tag int
+	nxt [26]*Trie
+	tag int
 }
 
-func (this *Trie) Insert(word string) {
-    root := this
-    for _, c := range word {
-        if root.nxt[c-'a'] == nil {
-            root.nxt[c-'a'] = &Trie{}
-        }
-        root = root.nxt[c-'a']
-    }
-    root.tag = 1
+func (tr *Trie) NewTrie() *Trie {
+	return &Trie{}
+}
+func (tr *Trie) Insert(word string) {
+	root := tr
+	for _, c := range word {
+		if root.nxt[c-'a'] == nil {
+			root.nxt[c-'a'] = &Trie{}
+		}
+		root = root.nxt[c-'a']
+	}
+	root.tag = 1
 }
 
-func (this *Trie) Search(word string) bool {
-    root := this
-    for _, c := range word {
-        if root.nxt[c-'a'] == nil {
-            return false
-        }
-        root = root.nxt[c-'a']
-    }
-    return root.tag == 1
+func (tr *Trie) Search(word string) bool {
+	root := tr
+	for _, c := range word {
+		if root.nxt[c-'a'] == nil {
+			return false
+		}
+		root = root.nxt[c-'a']
+	}
+	return root.tag == 1
 }
 
-func (this *Trie) StartsWith(prefix string) bool {
-    root := this
-    for _, c := range prefix {
-        if root.nxt[c-'a'] == nil {
-            return false
-        }
-        root = root.nxt[c-'a']
-    }
-    return true
+func (tr *Trie) StartsWith(prefix string) bool {
+	root := tr
+	for _, c := range prefix {
+		if root.nxt[c-'a'] == nil {
+			return false
+		}
+		root = root.nxt[c-'a']
+	}
+	return true
 }
