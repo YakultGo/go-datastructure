aca5dced451b18101ed524af32cdaa9078a16d98
YakultGo <god1239426256@163.com>
2024-04-16 17:53:58 +0800

    修改格式

--- datastructure/trie.go
+++ datastructure/trie.go
@@ -6,42 +6,42 @@ import "runtime/debug"
 func init() { debug.SetGCPercent(-1) }
 
 type Trie struct {
-	nxt [26]*Trie
-	tag int
+    nxt [26]*Trie
+    tag int
 }
 
 func (tr *Trie) NewTrie() *Trie {
-	return &Trie{}
+    return &Trie{}
 }
 func (tr *Trie) Insert(word string) {
-	root := tr
-	for _, c := range word {
-		if root.nxt[c-'a'] == nil {
-			root.nxt[c-'a'] = &Trie{}
-		}
-		root = root.nxt[c-'a']
-	}
-	root.tag = 1
+    root := tr
+    for _, c := range word {
+        if root.nxt[c-'a'] == nil {
+            root.nxt[c-'a'] = &Trie{}
+        }
+        root = root.nxt[c-'a']
+    }
+    root.tag = 1
 }
 
 func (tr *Trie) Search(word string) bool {
-	root := tr
-	for _, c := range word {
-		if root.nxt[c-'a'] == nil {
-			return false
-		}
-		root = root.nxt[c-'a']
-	}
-	return root.tag == 1
+    root := tr
+    for _, c := range word {
+        if root.nxt[c-'a'] == nil {
+            return false
+        }
+        root = root.nxt[c-'a']
+    }
+    return root.tag == 1
 }
 
 func (tr *Trie) StartsWith(prefix string) bool {
-	root := tr
-	for _, c := range prefix {
-		if root.nxt[c-'a'] == nil {
-			return false
-		}
-		root = root.nxt[c-'a']
-	}
-	return true
+    root := tr
+    for _, c := range prefix {
+        if root.nxt[c-'a'] == nil {
+            return false
+        }
+        root = root.nxt[c-'a']
+    }
+    return true
 }
