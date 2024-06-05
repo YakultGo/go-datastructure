1281b00d64651081ad33dfa001871a9210bc9882
YakultGo <god1239426256@163.com>
2024-06-05 13:54:04 +0800

    并查集的实现

---
+++ datastructure/dsu.go
@@ -0,0 +1,41 @@
+package datastructure
+
+type Dsu struct {
+	Fa     []int
+	Size   []int
+	Groups int
+}
+
+func NewDsu(n int) *Dsu {
+	fa := make([]int, n)
+	size := make([]int, n)
+	for i := 0; i < n; i++ {
+		fa[i] = i
+		size[i] = 1
+	}
+	return &Dsu{Fa: fa, Size: size, Groups: n}
+}
+func (u *Dsu) Find(x int) int {
+	if u.Fa[x] != x {
+		u.Fa[x] = u.Find(u.Fa[x])
+	}
+	return u.Fa[x]
+}
+
+func (u *Dsu) Union(x, y int) {
+	x = u.Find(x)
+	y = u.Find(y)
+	if x == y {
+		return
+	}
+	if u.Size[x] < u.Size[y] {
+		x, y = y, x
+	}
+	u.Fa[y] = x
+	u.Size[x] += u.Size[y]
+	u.Groups--
+}
+
+func (u *Dsu) Same(x, y int) bool {
+	return u.Find(x) == u.Find(y)
+}
