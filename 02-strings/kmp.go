cc6cccb87660fad3198e770a0fec9c37a1484d7b
YakultGo <god1239426256@163.com>
2024-10-21 20:22:40 +0800

    整理KMP实现

--- 02-strings/kmp.go
+++ 02-strings/kmp.go
@@ -1,35 +1,34 @@
-func prefixFunction(s string) []int {
-    n := len(s)
-    pi := make([]int, n)
-    for i := 1; i < n; i++ {
-        j := pi[i-1]
-        for j > 0 && s[i] != s[j] {
-            j = pi[j-1]
-        }
-        if s[i] == s[j] {
-            j++
-        }
-        pi[i] = j
-    }
-    return pi
+func nex(s string) []int {
+	n := len(s)
+	pi := make([]int, n)
+	for i := 1; i < n; i++ {
+		j := pi[i-1]
+		for j > 0 && s[i] != s[j] {
+			j = pi[j-1]
+		}
+		if s[i] == s[j] {
+			j++
+		}
+		pi[i] = j
+	}
+	return pi
 }
 
 func kmp(text, pattern string) []int {
-    pi := prefixFunction(pattern)
-    var res []int
-    c, m := 0, len(pattern)
-    for i := 0; i < len(text); i++ {
-        v := text[i]
-        for c > 0 && pattern[c] != v {
-            c = pi[c-1]
-        }
-        if pattern[c] == v {
-            c++
-        }
-        if c == m {
-            res = append(res, i-m+1)
-            c = pi[c-1]
-        }
-    }
-    return res
+	pi := nex(pattern)
+	var res []int
+	j, m := 0, len(pattern)
+	for i := 0; i < len(text); i++ {
+		for j > 0 && text[i] == pattern[j] {
+			j = pi[j-1]
+		}
+		if text[i] == pattern[j] {
+			j++
+		}
+		if j == m {
+			res = append(res, i-m+1)
+			j = pi[j-1]
+		}
+	}
+	return res
 }
